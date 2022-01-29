ca3799649b24e3ea4e0ff8f912426c1b19e952b8
allexradu <NeOac/\sa666H()mE>
2022-01-30 01:18:00 +0200

    synchronizing missed events

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,12 @@ const app = Express();
 app.use(Express.json());
 app.use(Express.urlencoded({extended: true}))
 
+const events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
+
+    events.push(event);
 
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:4001/events', event);
@@ -15,6 +19,10 @@ app.post('/events', (req, res) => {
 
     res.send({status: 'OK'});
 });
+
+app.get('/events', (req, res) => {
+    res.send(events);
+})
 
 app.listen(4005, () => {
     console.log('Listening on 4005')
--- moderation/index.js
+++ moderation/index.js
@@ -1,15 +1,18 @@
 import Express from 'express';
 import axios from "axios";
+import {sleep} from "../utils/index.js";
 
 const app = Express()
 app.use(Express.json())
 app.use(Express.urlencoded({extended: true}))
 
-app.post('/event', async (req, res) => {
+app.post('/events', async (req, res) => {
     const {type, data} = req.body;
 
     if (type === 'CommentCreated') {
         const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+        await sleep(10000);
 
         await axios.post('http://localhost:4005/events', {
             type: 'CommentModerated',
@@ -21,6 +24,7 @@ app.post('/event', async (req, res) => {
             }
         })
     }
+
 
     res.send({});
 })
